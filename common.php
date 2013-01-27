22639da0f98fc06ec8b77d72b38c4fa72eb9d173
Jim Blundell <james.blundell@keble.ox.ac.uk>
2013-01-27 07:36:21 +0000

    Added comment

--- common.php
+++ common.php
@@ -5,7 +5,7 @@
 	$mysql_pass = "";
 	$mysql_db = "";
 	$md5salt = "";
-	require_once "vars.php";
+	require_once "vars.php";	#Sets the above variables
 
 	$mysqli = new mysqli ($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
 	session_start ();
