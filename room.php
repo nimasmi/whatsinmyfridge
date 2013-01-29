545bb9e0fd77270629f2c43c5daf1ad4ce06f754
Nick Smith <nick@nicksmith.org>
2013-01-29 07:54:32 +0000

    added styling to the list of fridges in room.php

--- room.php
+++ room.php
@@ -19,21 +19,41 @@
     </div>
 
     <div class="span6"> <!-- main area -->
-<a href="addshelf.php?RoomID=<?php print $_REQUEST["ID"]; ?>">Add shelf</a><br>
-<?php
+<div class="row">
+    <div class="span3">
+        <h2><?php print $room_title; ?></h2>
+    </div>
+    <div class="span3">
+        <span class="pull-right"><a class="btn btn-primary" href="addshelf.php?RoomID=<?php print $_REQUEST["ID"]; ?>"><i class="icon-plus"></i> Add fridge</a></span>
+    </div>
+</div>
+    <?php
 	$room->close();
 
 	$shelf = $mysqli->prepare ("SELECT rooms.Title, shelves.Title, shelftypes.Title, labs.Title, labs.Latitude, labs.Longitude, labs.ID, rooms.ID, shelves.ID FROM shelves INNER JOIN shelftypes ON shelves.ShelfTypeID = shelftypes.ID INNER JOIN rooms ON shelves.RoomID = rooms.ID INNER JOIN labs ON rooms.LabID = labs.ID INNER JOIN institutions ON labs.InstitutionID = institutions.ID WHERE rooms.ID = ?");
 	$shelf->bind_param ("i", $_REQUEST["ID"]);
 	$shelf->bind_result ($room_title, $shelf_title, $shelf_type_title, $lab_title, $lat, $lng, $lab_id, $room_id, $shelf_id);
 	$shelf->execute ();
-
+?>
+        <ul class="media-list">
+<?php
 	while ($shelf->fetch()) {
 ?>
-<a href="shelf.php?ID=<?php print $shelf_id; ?>"><?php print $shelf_title; ?></a><br>
+            <li class="media">
+            <div class="well">
+                <a class="pull-left" href="#">
+                <img class="media-object" src="#FIXME">
+                </a>
+                <div class="media-body">
+                <h4 class="media-heading"><a href="shelf.php?ID=<?php print $shelf_id; ?>"><?php print $shelf_title; ?></a></h4>
+                <p><!-- FIXME some description can go here --></p>
+                </div>
+            </div>
+            </li>
 <?php
 	}
 ?>
+    </ul>
     </div> <!-- /span6 -->
     <div class="span3">
         <img src="img/icon-room.jpg">
